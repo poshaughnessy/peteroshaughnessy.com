c2db69b4d92a4d17c8efccdcfea6a7221438d93d
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-11-02 18:02:07 +0000

    RSS fixes

--- build.js
+++ build.js
@@ -34,7 +34,12 @@ function build() {
     }))
     .use(templates('handlebars'))
     .use(redirect(redirects))
-    .use(rssFeed({collection: 'posts', site_url: 'https://peteroshaughnessy.com'}))
+    .use(rssFeed({
+      collection: 'posts', 
+      site_url: 'https://peteroshaughnessy.com',
+      title: 'Peter O\'Shaughnessy',
+      description: 'Tech geek, web developer and advocate'
+    }))
     .destination('./build')
     .build(function(err) {
       if (err) throw err;
