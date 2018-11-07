34fbd0b5d852364ae88f3e602a95fdf8e6d42521
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2018-11-07 12:58:10 +0000

    Update dev server to use docs dir

--- dev-server.js
+++ dev-server.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const express = require('express'),
-  build = require('./build'),
+  docs = require('./docs'),
   PORT = process.env.PORT || 9000;
 
 build();
 
 let app = express();
 
-app.use( '/', express.static('build') );
+app.use( '/', express.static('docs') );
 
 app.use((req, res) => {
   res.status(404).send('Page not found');
